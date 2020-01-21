0fc4c3238f4ecccf0acca9bc3d22f262c58c3205
AlexLoitzl <56758218+AlexLoitzl@users.noreply.github.com>
2020-01-21 23:37:33 +0100

    Add files

---
+++ Trie.java
@@ -0,0 +1,102 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.HashMap;
+/**
+ * public class Trie
+ * 
+ * Stores words in a Tree, where each node represents a character and stores . The path from the root to this node
+ * forms a string being potentially a word.(if contained == true). Every node contains a HashMap storing key value pairs of nodes,
+ * instead of storing potential null pointers in arrays.
+ * 
+ * @author Alex
+ *
+ */
+public class Trie {
+	private class Node{
+		private HashMap<Character, Node> children;
+		private boolean contained;
+		private char letter;
+		
+		private Node(char c) {
+			children = new HashMap<Character, Node>();
+			letter = c;
+		}
+		private Node() {
+			children = new HashMap<Character, Node>();
+		}
+	}
+	private Node root;
+	
+	public Trie() {
+		root = new Node();
+	}
+	/**
+	 * Constructor used for reading a dictionary, which is required to have line seperated words
+	 * @param reader
+	 * @throws IOException
+	 */
+	public Trie(BufferedReader reader) throws IOException{
+		root = new Node();
+		readDictFromFile(reader);
+	}
+	/**
+	 * Used for reading a dictionary, which is required to have line seperated words
+	 * @param reader
+	 * @throws IOException
+	 */
+	public void readDictFromFile(BufferedReader reader) throws IOException{
+		String word;
+		while((word = reader.readLine())!=null) {
+			insert(word);
+		}
+	}
+	/**
+	 * Traverses through the tree character by character until the end of word. Creates HashMap entries on the way
+	 * @param word
+	 */
+	public void insert(String word) {
+		Node pointer = root;
+		word = word.toLowerCase();
+		for(int i = 0; i < word.length(); i++) {
+			pointer = pointer.children.computeIfAbsent(word.charAt(i), c -> new Node(c));
+		}
+		pointer.contained = true;
+	}
+	/**
+	 * Traverses through the Tree to find word
+	 * @param word
+	 * @return true if word was found, false otherwise
+	 */
+	public boolean find(String word) {
+		Node pointer = root;
+		for(int i = 0; i < word.length(); i++) {
+			char c = word.charAt(i);
+			Node node = pointer.children.get(c);
+			if(node == null) {
+				return false;
+			}
+			pointer = node;
+		}
+		return pointer.contained;
+	}
+	/**
+	 * Traverses through Tree to find a potential subword contained in word
+	 * @param word
+	 * @return index of last char of subword, -1 if not found
+	 */
+	public int findSubWord(String word) {
+		Node pointer = root;
+		for(int i = 0; i < word.length(); i++) {
+			char c = word.charAt(i);
+			Node node = pointer.children.get(c);
+			if(node == null) {
+				return -1;
+			}
+			pointer = node;
+			if(pointer.contained) {
+				return i;
+			}
+		}
+		return -1;
+	}
+}
---
+++ WordSeperator.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+/**
+ * public class WordSeperator
+ * 
+ * Provides Capability to split compound words. You an either instanciate it as an Object to use in another context,
+ * or run it on the Commandline expecting a line seperated dictionary as input. Uses public class Trie to store dictionary.
+ * 
+ * @author Alex
+ *
+ */
+public class WordSeperator {
+	/**
+	 * Returns all subwords of word as a String Array.
+	 * Traverses through the tree, if a subword was found, tries the rest, if not increases starting char.
+	 * 
+	 * @param word
+	 * @param trie
+	 * @return String Array of subwords
+	 */
+	public String[] trieSplit(String word, Trie trie) {
+		word = word.toLowerCase();
+		ArrayList<String> list = new ArrayList<String>();
+		int EndIndex;
+		while(word.length() > 1) {
+			EndIndex = trie.findSubWord(word);
+			if(EndIndex == -1) {
+				word = word.substring(1,word.length());
+			}else {
+				list.add(word.substring(0,EndIndex+1));
+				word = word.substring(EndIndex+1);
+			}
+		}
+		return (String[]) list.toArray(new String[0]);
+	}
+	/**
+	 * CLI
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		if(args.length != 0) {
+			System.out.println("Usage: WordSeparator <Dictionary>");
+		}else {
+			try (Scanner s = new Scanner(System.in); BufferedReader reader = new BufferedReader(new FileReader("german-common-nouns.txt", StandardCharsets.UTF_8))){
+				Trie dict = new Trie(reader);
+				reader.close();
+				String input = "";
+				WordSeperator sep = new WordSeperator();
+				while(!input.equals("q")) {
+					System.out.print("Enter Word to be separated or q to quit: ");
+					input = s.next().strip();
+					if(!input.equals("q")) {
+						System.out.println(Arrays.toString(sep.trieSplit(input,dict)));
+					}
+				}
+			}catch(FileNotFoundException e) {
+				System.out.println("File not found. Try rerunning with a different parameter");
+			}catch(IOException e) {
+				System.out.println("There was an issue handling file. Try rerunning with a different parameter");
+			}catch(InputMismatchException e) {
+				System.out.println("There was an issue with your input. Rerun the programm and try a different input");
+			}catch(Exception e) {
+				System.out.println("An error occured. Try rerunnning the programm");
+			}
+		}
+	}
+}
